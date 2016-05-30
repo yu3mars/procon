2e3ed090222cf462a7c7f146cf5e94b936b30c10
yu3mars <yu3mars@users.noreply.github.com>
2016-05-30 12:14:41 +0900

    add codeforces 484

---
+++ codeforces/484/a/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                long[] lr = Console.ReadLine().Split().Select(long.Parse).ToArray();
+                string l = Convert.ToString(lr[0], 2).PadLeft(60, '0');
+                string r = Convert.ToString(lr[1], 2).PadLeft(60, '0');
+                StringBuilder sb = new StringBuilder();
+                int same = 100;
+                for (int j = 0; j < l.Length; j++)
+                {
+                    if (j < same)
+                    {
+                        if (l[j] == r[j]) sb.Append(l[j]);
+                        else
+                        {
+                            sb.Append('0');
+                            same = j;
+                        }
+                    }
+                    else sb.Append("1");
+                }
+                long tmp = Convert.ToInt64(sb.ToString(), 2);
+                if (tmp < lr[0] || bitCount(tmp) < bitCount(lr[1])) tmp = lr[1];
+                Console.WriteLine(tmp);
+            }
+        }
+        static int bitCount(long x)
+        {
+            x = (x & 0x5555555555555555) + (x >> 1 & 0x5555555555555555);
+            x = (x & 0x3333333333333333) + (x >> 2 & 0x3333333333333333);
+            x = (x & 0x0f0f0f0f0f0f0f0f) + (x >> 4 & 0x0f0f0f0f0f0f0f0f);
+            x = (x & 0x00ff00ff00ff00ff) + (x >> 8 & 0x00ff00ff00ff00ff);
+            x = (x & 0x0000ffff0000ffff) + (x >> 16 & 0x0000ffff0000ffff);
+            return (int)((x & 0x00000000ffffffff) + (x >> 32 & 0x00000000ffffffff));
+        }
+
+
+    }
+}
---
+++ codeforces/484/b/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray().Distinct().ToArray().OrderBy(s => s).ToArray(); ;
+            int[] ls = new int[a[a.Length - 1] * 2 + 10];
+            int cnt = 0;
+            ls[0] = 0;
+            for (int i = 1; i < ls.Length; i++)
+            {
+                if(a[cnt]==i)
+                {
+                    ls[i] = a[cnt];
+                    cnt++;
+                    if (cnt >= a.Length) cnt = a.Length - 1;
+                }
+                else
+                {
+                    ls[i] = ls[i - 1];
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 1) continue;
+                for (int j = a[i] - 1; j < ls.Length; j += a[i])
+                {
+                    if (ls[j] > a[i])
+                    {
+                        ans = Math.Max(ans, ls[j] % a[i]);
+                    }
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
