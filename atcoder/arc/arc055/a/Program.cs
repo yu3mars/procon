675df6b437d9ac51f5270d374c4346cc324eb93d
yu3mars <yu3mars@users.noreply.github.com>
2016-06-04 15:04:54 +0900

    add arc055

---
+++ atcoder/arc/arc055/a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("1");
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write("0");
+            }
+            Console.WriteLine("7");
+        }
+    }
+}
