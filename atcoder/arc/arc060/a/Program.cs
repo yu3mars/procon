c579be28af6195c6fb1ca17556fb61a5880cfe7a
yu3mars <yu3mars@users.noreply.github.com>
2016-08-30 17:12:09 +0900

    add arc060

---
+++ atcoder/arc/arc060/a/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] na = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = na[0];
+            int a = na[1];
+            int[] x = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            long ans = 0;
+
+            Array.Sort(x);
+            for (int i = 0; i < n; i++)
+            {
+                x[i] -= a;
+            }
+            for (long i = 1; i < (1<<n); i++)
+            {
+                int tmp = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if((1 << j & i) != 0)
+                    {
+                        tmp += x[j];
+
+                    }
+                }
+                if(tmp==0)
+                {
+                    ans++;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ atcoder/arc/arc060/b/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = int.Parse(Console.ReadLine());
+            long s = int.Parse(Console.ReadLine());
+
+            if(s == 1)
+            {
+                Console.WriteLine(n);
+                return;
+            }
+            for (int i = 2; i < 100000; i++)
+            {
+                if( func(i,n)==s)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine(-1);
+        }
+        
+        static long func(long b,long n)
+        {
+            if (n < b) return n;
+            else return func(b, n / b) + (n % b); 
+        }
+    }
+}
