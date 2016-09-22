c2f8bf094de1305f149503e069fc2615a3b520ca
yu3mars <yu3mars@users.noreply.github.com>
2016-09-22 10:34:42 +0900

    add arc061

---
+++ atcoder/arc/arc061/a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            long ans = 0;
+
+            for (int bef = 0; bef < s.Length; bef++)
+            {
+                for (int len = 1; bef +len <= s.Length; len++)
+                {
+                    string substr = s.Substring(bef, len);
+                    int aft = s.Length - bef - len;
+                    long num = (long)(Math.Max(1, Math.Pow(2, bef - 1)) * 
+                        Math.Max(1, Math.Pow(2, aft - 1)));
+                    ans += long.Parse(substr) * num;
+                }
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
