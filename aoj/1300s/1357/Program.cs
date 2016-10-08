bb54245f81f9fe633b849757b196679225342f7d
yu3mars <yu3mars@users.noreply.github.com>
2016-10-08 19:11:30 +0900

    add aoj1357

---
+++ aoj/1300s/1357/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1357
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] y = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            double[] x = new double[n];
+            double xmax = 0;
+            for (int now = 0; now < n; now++)
+            {
+                double nowx = y[now];
+                for (int bef = 0; bef < now; bef++)
+                {
+                    nowx = Math.Max(nowx, x[bef] + Math.Sqrt(Math.Pow(y[now] + y[bef], 2) - Math.Pow(y[now] - y[bef], 2)));
+                }
+                x[now] = nowx;
+                xmax = Math.Max(xmax, x[now] + y[now]);
+            }
+            Console.WriteLine(xmax);
+        }
+    }
+}
