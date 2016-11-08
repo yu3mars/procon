f19eec0733062e5098b848377474a23d0f666d55
yu3mars <yu3mars@users.noreply.github.com>
2016-11-08 17:40:54 +0900

    add aoj 1149

---
+++ aoj/1100s/1149/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+//using System.Numerics;  //comment out if AOJ
+using System.Text;
+
+using Problem = Tmp.Problem;
+using MyIO;
+
+#pragma warning disable   //for AOJ
+
+namespace Tmp
+{
+    class Problem : IDisposable
+    {
+        bool IsGCJ;
+        int Repeat;
+        Scanner sc;
+        Printer pr;
+        public Problem(bool isGCJ, Scanner scanner, Printer printer)
+        {
+            sc = scanner;
+            pr = printer;
+            IsGCJ = isGCJ;
+            if (isGCJ) Repeat = sc.nextInt();
+            else Read();
+        }
+        public Problem(bool isGCJ) : this(isGCJ, new Scanner(), new Printer()) { }
+        public Problem(bool isGCJ, Scanner scanner) : this(isGCJ, scanner, new Printer()) { }
+        public Problem(bool isGCJ, Printer printer) : this(isGCJ, new Scanner(), printer) { }
+        public void Solve()
+        {
+            if (IsGCJ) for (var i = 0; i < Repeat; i++) { Read(); pr.Write("Case #" + (i + 1) + ": "); SolveOne(); }
+            else SolveOne();
+        }
+        public void Dispose()
+        {
+            sc.Dispose();
+            pr.Dispose();
+        }
+        public int Size { get { return 1; } }
+        public const long Mod = 1000000007;
+
+        // 使用する変数をここに書く
+        // string S;
+        // int a;
+        List<Cake> ls;
+        /// <summary>
+        /// 読み込み処理をここに書く
+        /// </summary>
+        void Read()
+        {
+
+        }
+        /// <summary>
+        /// メイン処理をここに書く
+        /// </summary>
+        void SolveOne()
+        {
+            for(;;)
+            {
+                int n = sc.nextInt();
+                int w = sc.nextInt();
+                int d = sc.nextInt();
+                if (w == 0) return;
+                ls = new List<Cake>();
+                ls.Add(new Cake(w, d));
+                for (int i = 0; i < n; i++)
+                {
+                    int p = sc.nextInt() - 1;
+                    int s = sc.nextInt();
+                    Cake[] v = ls[p].divide(s);
+                    ls.Add(v[0]);
+                    ls.Add(v[1]);
+                    ls.RemoveAt(p);
+                }
+                int[] ans = new int[ls.Count];
+                for (int i = 0; i < ls.Count; i++)
+                {
+                    ans[i] = ls[i].area();
+                }
+                Array.Sort(ans);
+                for (int i = 0; i < ls.Count; i++)
+                {
+                    if (i > 0) Console.Write(" ");
+                    Console.Write(ans[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        struct Cake
+        {
+            public int w;
+            public int h;
+            public Cake(int w,int h)
+            {
+                this.w = w;
+                this.h = h;
+            }
+            public int area()
+            {
+                return w * h;
+            }
+            public Cake[] divide(int n)
+            {
+                Cake[] ret = new Cake[2];
+                n %= (w + h);
+                if (n < w)
+                {
+                    int minW = Math.Min(n, w - n);
+                    int maxW = w - minW;
+                    ret[0] = new Cake(minW, h);
+                    ret[1] = new Cake(maxW, h);
+                }
+                else
+                {
+                    n -= w;
+                    int minH = Math.Min(n, h - n);
+                    int maxH = h - minH;
+                    ret[0] = new Cake(w, minH);
+                    ret[1] = new Cake(w, maxH);
+                }
+                return ret;
+            }
+        }
+    }
+}
+class Program
+{
+    //public static RandomSFMT rand = new RandomSFMT();
+    public static bool IsJudgeMode = true;
+    public static bool IsGCJMode = false;
+    public static bool IsSolveCreated = true;
+    static void Main()
+    {
+        if (IsJudgeMode)
+            if (IsGCJMode) using (var problem = new Problem(true, new Scanner("C-large-practice.in.txt"), new Printer("output.txt"))) problem.Solve();
+            else using (var problem = new Problem(false, new Printer())) problem.Solve();
+        else
+        {
+            var num = 1;
+            int size = 0;
+            decimal time = 0;
+            for (var tmp = 0; tmp < num; tmp++)
+            {
+                using (var P = IsSolveCreated ? new Problem(false, new Scanner("input.txt"), new Printer()) : new Problem(false))
+                {
+                    size = P.Size;
+                    //time += Func.MeasureTime(() => P.Solve());
+                }
+            }
+            Console.WriteLine("{0}, {1}ms", size, time / num);
+        }
+    }
+}
+
+/// <summary>
+/// カスタマイズしたIO
+/// </summary>
+namespace MyIO
+{
+    class Printer : IDisposable
+    {
+        bool isConsole;
+        TextWriter file;
+        public Printer() { file = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false }; isConsole = true; }
+        public Printer(string path) { file = new StreamWriter(path, false) { AutoFlush = false }; isConsole = false; }
+        public void Write<T>(T value) { file.Write(value); }
+        public void Write(bool b) { file.Write(b ? "YES" : "NO"); }
+        public void Write(string str, params object[] args) { file.Write(str, args); }
+        public void WriteLine() { file.WriteLine(); }
+        public void WriteLine<T>(T value) { file.WriteLine(value); }
+        public void WriteLine(bool b) { file.WriteLine(b ? "YES" : "NO"); }
+        public void WriteLine<T>(IEnumerable<T> list) { foreach (var x in list) file.WriteLine(x); }
+        public void WriteLine<T>(List<T> list) { foreach (var x in list) file.WriteLine(x); }
+        public void WriteLine<T>(T[] list) { foreach (var x in list) file.WriteLine(x); }
+        public void WriteLine(string str, params object[] args) { file.WriteLine(str, args); }
+        public void Dispose() { file.Flush(); if (!isConsole) file.Dispose(); }
+    }
+    class Scanner : IDisposable
+    {
+        bool isConsole;
+        TextReader file;
+        public Scanner() { file = Console.In; }
+        public Scanner(string path) { file = new StreamReader(path); isConsole = false; }
+        public void Dispose() { if (!isConsole) file.Dispose(); }
+
+        #region next読み込み
+        string[] nextBuffer = new string[0];
+        int BufferCnt = 0;
+
+        char[] cs = new char[] { ' ' };
+
+        public string next()
+        {
+            while (BufferCnt >= nextBuffer.Length)
+            {
+                string st = file.ReadLine();
+                while (st == "") st = file.ReadLine();
+                nextBuffer = st.Split(cs, StringSplitOptions.RemoveEmptyEntries);
+                BufferCnt = 0;
+            }
+            return nextBuffer[BufferCnt++];
+        }
+
+        public int nextInt()
+        {
+            return int.Parse(next());
+        }
+
+        public long nextLong()
+        {
+            return long.Parse(next());
+        }
+
+        public double nextDouble()
+        {
+            return double.Parse(next());
+        }
+
+        private T[] enumerate<T>(int n, Func<T> f)
+        {
+            var a = new T[n];
+            for (int i = 0; i < n; ++i) a[i] = f();
+            return a;
+        }
+
+        public string[] next(int n) { return enumerate(n, next); }
+        public double[] nextDouble(int n) { return enumerate(n, nextDouble); }
+        public int[] nextInt(int n) { return enumerate(n, nextInt); }
+        public long[] nextLong(int n) { return enumerate(n, nextLong); }
+        #endregion
+    }
+}
