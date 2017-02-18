d785a005a878203472c1fe0642540604dfc7d2ac
yu3mars <yu3mars@users.noreply.github.com>
2017-02-18 20:56:15 +0900

    add srm708div2

---
+++ topcoder/srm708/easy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SafeBetting.minRounds(1,2,1000));
+        }
+    }
+}
+class SafeBetting
+{
+    public static int minRounds(int a, int b, int c)//saitei,temochi,goal
+    {
+        int kake = b - a;
+        int now = b;
+        int ans = 0;
+        while(now < c)
+        {
+            ans++;
+            now += kake;
+            kake = now - a;
+        }
+        return ans;
+    }
+}
